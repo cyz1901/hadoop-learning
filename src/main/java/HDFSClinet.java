dcc9fa9b7667d821b55cf47f1890c223f4c5c88d
cyz12 <1093491992@qq.com>
2020-04-21 21:15:33 +0800

    one

--- src/main/java/HDFSClinet.java
+++ src/main/java/HDFSClinet.java
@@ -1,11 +1,15 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class HDFSClinet {
-    public static void main(String[] args) throws IOException {
+    @Test
+    public void connecthdfs_one() throws IOException {
         Configuration conf = new Configuration();
         conf.set("fs.defaultFS","hdfs://192.168.1.106:9000");
         //获取hdfs客户端对象
@@ -16,5 +20,58 @@ public class HDFSClinet {
         //关闭资源
         fs.close();
         System.out.println("over");
+    }
+
+    @Test
+    public void connecthdfs_two() throws URISyntaxException, IOException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        fs.mkdirs(new Path("/0529/dashen"));
+
+        //关闭资源
+        fs.close();
+        System.out.println("over");
+    }
+
+    @Test
+    public void testCopyFromLocalFile() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        fs.copyFromLocalFile(new Path("src/main/resources/i have a dream.txt"),new Path("/test.txt"));
+
+        fs.close();
+        System.out.println("over");
+    }
+
+    @Test
+    public void testCopyToLocalFile() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        fs.copyToLocalFile(new Path("/test.txt"), new Path("D:/happy.txt"));
+
+        fs.close();
+        System.out.println("over");
+    }
+
+    @Test
+    public void testDelete() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        fs.delete(new Path("/test.txt"),true);
+
+        fs.close();
+        System.out.println("over");
+    }
+
+    public static void main(String[] args) throws IOException {
+
     }
 }
