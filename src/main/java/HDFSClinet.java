2c21acc87404bc2c7ce977c3c7f156f83a614fd9
cyz12 <1093491992@qq.com>
2020-04-22 15:15:21 +0800

    文件操作

--- src/main/java/HDFSClinet.java
+++ src/main/java/HDFSClinet.java
@@ -1,8 +1,11 @@
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -71,6 +74,66 @@ public class HDFSClinet {
         System.out.println("over");
     }
 
+    @Test
+    public void testRename() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        fs.rename(new Path("/test.txt"),new Path("/happy.txt"));
+
+        fs.close();
+        System.out.println("over");
+    }
+
+
+    @Test
+    public void testListFile() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        RemoteIterator<LocatedFileStatus> filelist =  fs.listFiles(new Path("/"),true);
+
+        while (filelist.hasNext()){
+            LocatedFileStatus filestatus = filelist.next();
+            System.out.println(filestatus.getPath().getName());
+            System.out.println(filestatus.getPermission());
+            System.out.println(filestatus.getBlockSize());
+            BlockLocation[] blockLocations = filestatus.getBlockLocations();
+
+            for (BlockLocation blockLocation : blockLocations){
+                String[] hosts = blockLocation.getHosts();
+                for (String host : hosts){
+                    System.out.println(host);
+                }
+            }
+
+        }
+
+
+        fs.close();
+        System.out.println("over");
+    }
+
+    @Test
+    public void putFileToHDFS() throws IOException, URISyntaxException, InterruptedException {
+        Configuration configuration =new Configuration();
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.106:9000"),configuration,"hadoop");
+
+        FileInputStream fileInputStream= new FileInputStream(new File("D:/happy.txt"));
+
+        FSDataOutputStream fileOutputStream = fs.create(new Path("/happy0.txt"));
+
+        IOUtils.copyBytes(fileInputStream,fileOutputStream,configuration);
+        IOUtils.closeStream(fileOutputStream);
+        IOUtils.closeStream(fileInputStream);
+
+
+        fs.close();
+        System.out.println("over");
+    }
     public static void main(String[] args) throws IOException {
 
     }
